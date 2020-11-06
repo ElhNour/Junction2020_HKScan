0478469bf15c0923e1303207afdd0e86e3556287
Nour EL HASSANE <hn_el_hassane@esi.dz>
2020-11-06 21:15:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ButtonAppBar></ButtonAppBar>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
